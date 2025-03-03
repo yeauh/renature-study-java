643a70d5070fc52c4733a8e5f68525245cb1ed0d
alexYea <alexanderterekhov1@gmail.com>
2025-03-03 16:08:15 -0500

    Method overloading

--- src/Adder.java
+++ src/Adder.java
@@ -8,3 +8,15 @@ class testOverLoading1 {
         System.out.println(Adder.add(1, 2, 4));
     }
 }
+
+// Method Overloading - Changing data type of arguments
+class Adder2 {
+    static int add(int a, int b) {return a + b;}
+    static double add(double a, double b) {return a + b;} // 2 arguments of double data type
+}
+class testOverLoading2 {
+    public static void main(String[] args) {
+        System.out.println(Adder2.add(11, 11));
+        System.out.println(Adder2.add(2.2, 2.2));
+    }
+}
