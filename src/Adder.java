143626c2ed9af4fa3a63834dbcab6f8c2e402378
alexYea <alexanderterekhov1@gmail.com>
2025-03-03 16:18:24 -0500

    Method Overriding

--- src/Adder.java
+++ src/Adder.java
@@ -28,3 +28,28 @@ class TestOverLoading4 {
     public static void main(String args){System.out.println("main with String");}
     public static void main() {System.out.println("main without args");}
 }
+
+// Method OverRIDING
+
+class ABC {
+    // Overridden method
+    public void disp()
+    {
+        System.out.println("disp() method of parent class");
+    }
+}
+class Demo extends ABC {
+    // Overriding method
+    public void disp() {
+        System.out.println("disp() method of Child class");
+    }
+    public void newMethod() {
+        System.out.println("new method of child class");
+    }
+    public static void main(String[] args) {
+        ABC obj = new ABC();
+        obj.disp();
+        ABC obj2 = new Demo();
+        obj2.disp();
+    }
+}
