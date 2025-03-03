1f81b7e805984f647101879ecdffe8e9a7226981
alexYea <alexanderterekhov1@gmail.com>
2025-03-03 16:11:49 -0500

    Overloading the main() method

--- src/Adder.java
+++ src/Adder.java
@@ -20,3 +20,11 @@ class testOverLoading2 {
         System.out.println(Adder2.add(2.2, 2.2));
     }
 }
+
+// Overloading the main() method
+// Java would only run the main class that receives string arrays as arguments
+class TestOverLoading4 {
+    public static void main(String[] args) {System.out.println("main with String[]");}
+    public static void main(String args){System.out.println("main with String");}
+    public static void main() {System.out.println("main without args");}
+}
