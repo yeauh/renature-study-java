5e36028336bc9714b71d750a6c2539722663f140
alexYea <alexanderterekhov1@gmail.com>
2025-03-04 13:33:43 -0500

    Exercise files

---
+++ src/Bank_Exercise.java
@@ -0,0 +1,3 @@
+public class Bank_Exercise {}
+
+// Consider a scenario where Bank is a class that provides functionality to get the rate of interest. However, the rate of interest varies according to banks. For example, SBI, ICICI and AXIS banks could provide 8%, 7%, and 9% rate of interest.
