d18ccbcc051d9afc0953c0c9ba9d937b6c5e9fc3
alexYea <alexanderterekhov1@gmail.com>
2025-03-06 12:19:17 -0500

    conditional statements

---
+++ src/conditional_statements.java
@@ -0,0 +1,14 @@
+// https://sites.google.com/revature.com/studyguide/java/conditional-statements?authuser=0
+
+//public class conditional_statements {}
+
+class Test {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        } else {
+            System.out.println("x is less than or equal to 5");
+        }
+    }
+}
