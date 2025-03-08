053db5284c962e332765546ec78118c009697022
alexYea <alexanderterekhov1@gmail.com>
2025-03-08 16:13:59 -0500

    While loop

--- src/looping_constructs.java
+++ src/looping_constructs.java
@@ -1,2 +1,22 @@
-public class looping_constructs {
+import java.util.Scanner;
+
+public class looping_constructs { }
+
+/*
+* Types of Loops:
+* While loop
+* For Loop
+* Do...While Loops*/
+
+// While loop
+
+class Test1 {
+    public static void main(String[] args) {
+        int x = 10;
+        while (x <= 20) {
+            System.out.print("Value of x: " + x);
+            x++;
+            System.out.println();
+        }
+    }
 }
