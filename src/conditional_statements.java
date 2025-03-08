77f4e8ce854e48295610eca40ad060d7aeeb7f5a
alexYea <alexanderterekhov1@gmail.com>
2025-03-08 12:31:27 -0500

    Incorporating JOptionPane for a GUI input

--- src/conditional_statements.java
+++ src/conditional_statements.java
@@ -10,6 +10,7 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
+// Incorporating JOptionPane for a GUI input on line 19
 class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
