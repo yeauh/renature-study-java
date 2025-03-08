594475f9411aa65b38daedcdd57397756a57e43b
alexYea <alexanderterekhov1@gmail.com>
2025-03-08 16:15:46 -0500

    For loop

--- src/looping_constructs.java
+++ src/looping_constructs.java
@@ -20,3 +20,13 @@ class Test1 {
         }
     }
 }
+
+class Test2 {
+    public static void main(String[] args) {
+        System.out.println("For loop\n");
+        for (int x = 10; x < 20; x++) {
+            System.out.print("Value of x: " + x);
+            System.out.println();
+        }
+    }
+}
