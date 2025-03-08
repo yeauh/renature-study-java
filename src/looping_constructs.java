aa3071e68eea3bfd107a7de2d3dbee8c761f04c6
alexYea <alexanderterekhov1@gmail.com>
2025-03-08 16:37:07 -0500

    Enhanced for loop (2)

--- src/looping_constructs.java
+++ src/looping_constructs.java
@@ -54,5 +54,12 @@ class Test4 {
             System.out.print(", ");
         }
         System.out.println();
+
+        String[] names = {"James", "Larry", "Tom", "Lacy"};
+
+        for (String name : names) {
+            System.out.print(name);
+            System.out.print(", ");
+        }
     }
 }
