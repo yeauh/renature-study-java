b371cf74ff9d89e161873a4fbca197c4ba8905ab
alexYea <alexanderterekhov1@gmail.com>
2025-03-08 12:31:07 -0500

    Switch statement

--- src/conditional_statements.java
+++ src/conditional_statements.java
@@ -30,3 +30,25 @@ class Test {
         }
     }
 }
+
+class switchTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        String x = sc.nextLine();
+        switch (x) {
+            case "1":
+                System.out.println("You entered 1");
+                break;
+            case "2":
+                System.out.println("You entered 2");
+                break;
+            case "3":
+                System.out.println("You entered 3");
+                break;
+            default:
+                System.out.println("You entered the number " + x);
+        }
+
+    }
+}
